3b9e73a7b664d326bd7e974b9cffa1977e970f76
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-10-14 12:31:16 -0500

    Added not-found
    Updated the not-found page that displays when a user tries to go to a page that does not exist.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,14 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link className='underline' href="/">Return Home</Link>
+    <div className='flex items-center justify-center px-4 h-[90vh]' >
+      <div className=' bg-red-300/[.5] w-full max-w-[400px] h-[150px] px-4 rounded flex items-center justify-center'>
+        <div className=''>
+          <h2>Not Found</h2>
+          <p>Could not find requested resource</p>
+          <Link className='underline' href="/">Return Home</Link>
+        </div>
+      </div>
     </div>
   )
 }
