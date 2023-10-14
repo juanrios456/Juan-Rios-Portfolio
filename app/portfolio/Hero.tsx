277b69b022d43d9dd78810e9af629a8acee46a11
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-10-14 18:53:35 -0500

    Add files via upload

--- app/portfolio/Hero.tsx
+++ app/portfolio/Hero.tsx
@@ -1,17 +1,20 @@
+import { RevealOnScroll } from "@components/ScrollTransitions";
 import Image from "next/image";
 
 export function Hero(){
   return(
-  <div className="relative h-[500px] " >
+  <div className="relative h-[95vh] bg-black " >
     <video className="w-full h-full object-cover absolute object-left-bottom"  autoPlay muted loop>
       <source src="/portfolio/rotating-earth.mp4" type="video/mp4"/>
       Your browser does not support the video tag.
     </video>
     <div className="h-full w-full flex items-center relative justify-end right-auto md:right-36 md:absolute ">
-      <div className="px-4 rounded-md max-w-[400px] space-y-5 ">
+      <RevealOnScroll>
+      <div className="p-4 rounded-lg max-w-[400px] space-y-5 bg-black/[.3] ">
         <h1 className="text-center" >Portfolio</h1>
         <p className="">Here you will find a list of skills and my work experiences, as well as a couple of projects and their tech stacks</p>
       </div>
+      </RevealOnScroll>
     </div>
     
   </div>
